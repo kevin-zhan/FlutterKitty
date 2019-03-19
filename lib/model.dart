40fd40c8d0ccd530398ab91b2b3cfe3ccb824a43
kevin-zhan <kevinzhan0417@outlook.com>
2019-03-19 20:29:05 +0800

    change to kitty style

--- lib/model.dart
+++ lib/model.dart
@@ -3,54 +3,13 @@ import 'package:json_annotation/json_annotation.dart';
 part 'model.g.dart';
 
 @JsonSerializable(nullable: false)
-class StoryModel {
-  final List<String> images;
-  final int id;
-  final String title;
-  StoryModel({this.images, this.id, this.title});
-  factory StoryModel.fromJson(Map<String, dynamic> json) => _$StoryModelFromJson(json);
-  Map<String, dynamic> toJson() => _$StoryModelToJson(this);
-}
+class KittyModel {
+  final String id;
+  final String url;
 
-@JsonSerializable(nullable: false)
-class StoryListModel {
-  final List<StoryModel> stories;
-  StoryListModel(this.stories);
-  factory StoryListModel.fromJson(Map<String, dynamic> json) => _$StoryListModelFromJson(json);
-  Map<String, dynamic> toJson() => _$StoryListModelToJson(this);
-}
+  KittyModel({this.id, this.url});
 
-@JsonSerializable()
-class StoryContentModel {
-  final int id;
-  final int type;
-  final String title;
-  final String body;
-  final List<String> js;
-  final List<String> css;
-  final String share_url;
-  final List recommenders;
-  final String ga_prefix;
-  final String image_source;
-  final String image;
-  final List<String> images;
+  factory KittyModel.fromJson(Map<String, dynamic> json) => _$KittyModelFromJson(json);
 
-  StoryContentModel(
-      {this.id,
-        this.type,
-        this.title,
-        this.body,
-        this.js,
-        this.css,
-        this.share_url,
-        this.recommenders,
-        this.ga_prefix,
-        this.image_source,
-        this.image,
-        this.images});
-
-
-  factory StoryContentModel.fromJson(Map<String, dynamic> json) => _$StoryContentModelFromJson(json);
-
-  Map<String, dynamic> toJson() => _$StoryContentModelToJson(this);
+  Map<String, dynamic> toJson() => _$KittyModelToJson(this);
 }
--- lib/view_model.dart
+++ lib/view_model.dart
@@ -1,73 +1,31 @@
 import 'dart:async';
 import 'model.dart';
 import 'network_repo.dart';
-import 'story_renderer.dart';
 
-class StoryListViewModel {
-  var _storyListController = StreamController<StoryListModel>.broadcast();
-  List<StoryModel> storyList = List();
+class KittyListViewModel {
+  var _storyListController = StreamController<List<KittyModel>>.broadcast();
+  List<KittyModel> kittyList = List();
   var offset = -1;
 
-  Sink get inStoryListController => _storyListController;
+  Sink get inKittyListController => _storyListController;
 
-  Stream<List<StoryModel>> get outStoryList => _storyListController.stream.map((stories) {
-    storyList.addAll(stories.stories);
-    return storyList;
+  Stream<List<KittyModel>> get outStoryList => _storyListController.stream.map((kitties) {
+    kittyList.addAll(kitties);
+    return kittyList;
   });
 
-  refreshStoryList() async {
-    offset = 0;
-    storyList.clear();
-    StoryListModel model = await NetWorkRepo.requestNewsList(offset);
-    inStoryListController.add(model);
+  refreshKittyList() async {
+    kittyList.clear();
+    List<KittyModel> list = await NetWorkRepo.requestKittyList();
+    inKittyListController.add(list);
   }
 
   loadNextPage() async {
-    offset++;
-    StoryListModel model = await NetWorkRepo.requestNewsList(offset);
-    inStoryListController.add(model);
+    List<KittyModel> list = await NetWorkRepo.requestKittyList();
+    inKittyListController.add(list);
   }
 
   depose() {
     _storyListController.close();
   }
-}
-
-class StoryContentViewModel {
-  int storyId;
-
-  StoryContentViewModel(this.storyId);
-
-  var _storyContentController = StreamController<StoryContentModel>.broadcast();
-
-  Sink get inStoryContentController => _storyContentController;
-
-  Stream<StoryContentModel> get outStoryContent => _storyContentController.stream.map((storyContent) {
-    return storyContent;
-  });
-
-  Stream<String> get outStoryTitle => _storyContentController.stream.map((storyContent) {
-    if (storyContent == null) {
-      return "加载中";
-    }
-    return storyContent.title;
-  });
-
-  Stream<String> get outStoryHtml => _storyContentController.stream.asyncMap((storyContent) {
-    if (storyContent == null) {
-      return "";
-    }
-    return makeStoryContextHtml(storyContent);
-
-  });
-
-  fetchStoryContent() async {
-    StoryContentModel contentModel = await NetWorkRepo.requestNewsContent(storyId);
-    inStoryContentController.add(contentModel);
-  }
-
-  depose() {
-    _storyContentController.close();
-  }
-
 }
