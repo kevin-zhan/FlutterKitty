4e524091bb4b2a175bd06f05534c6eaa08eb685b
kevin-zhan <kevinzhan0417@outlook.com>
2019-06-19 20:01:59 +0800

    Remove ignored files

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/kevinzhan/code/git_repo/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
---
+++ lib/model.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+KittyModel _$KittyModelFromJson(Map<String, dynamic> json) {
+  return KittyModel(id: json['id'] as String, url: json['url'] as String);
+}
+
+Map<String, dynamic> _$KittyModelToJson(KittyModel instance) =>
+    <String, dynamic>{'id': instance.id, 'url': instance.url};
