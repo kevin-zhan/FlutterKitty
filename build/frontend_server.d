4b89281d8a188fed7fc5571ac8371db717668f6b
湛杨梦晓 <kevinzhan0417@outlook.com>
2019-10-03 16:40:53 +0800

    update .gitignore

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /Users/kevinzhan/code/git_repo/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
+frontend_server.d: /Users/kevin/code/git_tool/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
